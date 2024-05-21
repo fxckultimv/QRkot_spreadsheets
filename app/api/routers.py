b7f281693d25a8deb9454cd7cc522d0d8fe9d8df
Mikhail Komissarov <datradexon@gmail.com>
2024-05-21 19:30:38 +0300

    v 1.2

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import charityproject_router, donations, users
+from app.api.endpoints.google_api import google_api_router
+
 from const import (USER_PREFIX, DONATION_PREFIX, CHARITY_PROJECT_PREFIX,
-                   USER_TAGS, DONATION_TAGS, CHARITY_PROJECT_TAGS)
+                   USER_TAGS, DONATION_TAGS, CHARITY_PROJECT_TAGS,
+                   GOOGLE_PREFIX, GOOGLE_TAGS)
 
 main_router = APIRouter()
 
@@ -22,4 +25,10 @@ main_router.include_router(
     charityproject_router.router,
     prefix=CHARITY_PROJECT_PREFIX,
     tags=CHARITY_PROJECT_TAGS
+)
+
+main_router.include_router(
+    google_api_router,
+    prefix=GOOGLE_PREFIX,
+    tags=GOOGLE_TAGS
 )
